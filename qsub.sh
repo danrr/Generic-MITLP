455c81d804754c83a9ac104e7199f77477ab414e
Dan Ristea <danristea@protonmail.com>
2023-07-25 21:47:34 +0100

    add submission file

---
+++ qsub.sh
@@ -0,0 +1,23 @@
+# These are flags you must include - Two memory and one runtime.
+# Runtime is either seconds or hours:min:sec
+
+#$ -l tmem=1G
+#$ -l h_vmem=1.5G
+#$ -l h_rt=100000
+#$ -pe smp 1
+#$ -R y
+#$ -wd /home/dristea/projects/Generic-MITLP
+
+#These are optional flags but you probably want them in all jobs
+
+#$ -S /bin/bash
+#$ -j y
+#$ -N benchmarkCycles
+
+#The code you want to run now goes here.
+
+export PATH=/share/apps/python-3.9.5-shared/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/python-3.9.5-shared/lib:$LD_LIBRARY_PATH
+
+source ./.venv/bin/activate
+python benchmarks/benchmark_squarings.py
