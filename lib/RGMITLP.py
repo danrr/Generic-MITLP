7e0492ff127ad279dde3ab1d3e40f8be4a3c5725
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-06-26 12:52:24 +0100

    (feat: web3) starting a local test network if no network provider is defined

--- lib/RGMITLP.py
+++ lib/RGMITLP.py
@@ -26,12 +26,28 @@ class RGMILTP:
         print(compiled_sol)
 
         self.abi = compiled_sol[CONTRACT_PATH+":"+CONTRACT_NAME]["abi"]
+        
+        assert self.abi is not None
+
+
+    """
+        Initiates the network connection
+        @param provider: The provider to use for the connection
+    """
+    def initiate_network(self, provider=None):
+
+        # If no provider is given, use the EthereumTesterProvider which runs a local testnet
+        if provider is None:
+            provider = EthereumTesterProvider(ethereum_tester=EthereumTester(backend=PyEVMBackend()))
+
+        self.web3 = Web3(provider)
+
+        assert self.web3.is_connected()
 
 
     def __init__(self):
         self.load_abi()
-        pass
-
+        self.initiate_network()
 
 
 if __name__ == "__main__":
