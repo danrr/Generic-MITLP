a8123564ca230074d70a87250c0e7b09cc80ed89
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-06-26 22:13:01 +0100

    (feat: web3) added a SC Interface class

---
+++ lib/smartcontracts/SCInterface.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+
+class SCInterface(ABC):
+
+    @abstractmethod
+    def __init__(self, coins, start_time, extra_time, upper_bounds, helper_id):
+        pass
+
+    @abstractmethod
+    def initiate(self, coins, start_time, extra_time, upper_bounds, helper_id):
+        pass
+
+    @abstractmethod
+    def add_solution(self, solution, witness):
+        pass
+
+    @abstractmethod
+    def get_message_at(self, i):
+        pass
+
+    @property
+    @abstractmethod
+    def upper_bounds(self):
+        pass
+
+    @upper_bounds.setter
+    @abstractmethod
+    def upper_bounds(self, upper_bounds):
+        pass
+
+    @property
+    @abstractmethod
+    def coins(self):
+        pass
+
+    @coins.setter
+    @abstractmethod
+    def coins(self, upper_bounds):
+        pass
+
+    @property
+    @abstractmethod
+    def start_time(self):
+        pass
+
+    @start_time.setter
+    @abstractmethod
+    def start_time(self, start_time):
+        pass
+
+    @property
+    @abstractmethod
+    def commitments(self):
+        pass
+
+    @commitments.setter
+    @abstractmethod
+    def commitments(self, commitments):
+        pass
